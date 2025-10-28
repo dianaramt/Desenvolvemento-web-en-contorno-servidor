01dda09dedc5214e2f2f6ceeda7ef87623a10f02
Diana <dianaramos19@gmail.com>
2025-10-28 12:38:27 +0100

    subimos de novo a tarea1 con todo o codigo

---
+++ Tema1/tarea1/www/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Módulo DWCS</title>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <?php
+            echo "<h2>Hola! Bienvenido al módulo de DWCS. Mi nombre es Diana!</h2>";
+        ?>
+    </body>
+</html>
+
