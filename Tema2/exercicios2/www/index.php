3fcabb8aeb8ce41b1aba320aadd9bd3641c61a9e
Diana <dianaramos19@gmail.com>
2025-10-28 12:51:35 +0100

    comezando a facer os exercicios do tema2

---
+++ Tema2/exercicios2/www/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Módulo DWCS</title>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <?php
+            echo "<h1>Estes son os exercicios2 do tema2 resoltos por min</h1>";
+        ?>
+    </body>
+</html>
+
