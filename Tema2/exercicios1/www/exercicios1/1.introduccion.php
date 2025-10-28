464144350774ed99454df759fdcaa068d1080328
Diana <dianaramos19@gmail.com>
2025-10-28 12:53:13 +0100

    engadidos exercicios1 do tema2 sen resolver ainda

---
+++ Tema2/exercicios1/www/exercicios1/1.introduccion.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Exercicio 1</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+        
+
+        </div>  
+    </body>
+</html>
